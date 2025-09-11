b48a9680b64d4c6898b8b32e561b099549a6d2c0
이형석 <91.valyrian@gmail.com>
2025-09-11 16:52:34 +0900

    mode setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./src/app/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: "class", // 미디어 쿼리 대신 클래스 기반 다크 모드
   theme: {
     extend: {},
   },
