7bcc85ee83d3a02842ad468f35bd376de631e2cb
이형석 <91.valyrian@gmail.com>
2025-09-11 16:46:49 +0900

    다크모드 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/app/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: "class", // 미디어 쿼리 대신 클래스 기반 다크 모드
   theme: {
     extend: {},
   },
