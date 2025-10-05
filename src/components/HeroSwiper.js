a1d2e9fe47107c7385e5ed0977aeae7750a5c8ab
이형석 <91.valyrian@gmail.com>
2025-10-06 02:27:16 +0900

    hero padding bottom edit

--- src/components/HeroSwiper.js
+++ src/components/HeroSwiper.js
@@ -218,7 +218,7 @@ export default function HeroSwiper() {
       </Swiper>
 
       {/* 네비게이션 버튼 */}
-      <div className="container absolute left-1/2 translate-x-[-50%] -bottom-[-120px] md:-bottom-[-50px] z-50 flex justify-between">
+      <div className="container absolute left-1/2 translate-x-[-50%] -bottom-[-60px] md:-bottom-[-50px] z-50 flex justify-between">
         <div className="scroll flex items-center">
           <div className="scroll-ico flex gap-[20px] md:gap-[10px] items-center">
             <div className="ico relative rounded-[9999px] w-[80px] h-[120px] md:w-[50px] md:h-[80px] border-[2px] border-white flex flex-col items-center justify-center overflow-hidden">
